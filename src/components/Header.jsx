633db81f6c43532743ced667eb8c6ce288ac1200
Ana Vingada <anavingada@gmail.com>
2024-09-03 13:51:49 +0100

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import logo from '../assets/investment-calculator-logo.png'
+
+export default function Header() {
+  return (
+    <header id="header">
+      <figure>
+        <img
+          src={logo}
+          alt="Investment Calculator logo"
+        />
+      </figure>
+      <h1>React Investment Calculator</h1>
+    </header>
+  );
+}
